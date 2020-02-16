7b29c44d66f22da0245e9403aefc26c1f3fe027a
Zachary P. Christensen <zchristensen7@gmail.com>
2020-02-15 21:36:11 -0500

    Clean up broadcast tests

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -8,47 +8,53 @@ bstyle = Base.Broadcast.DefaultArrayStyle{1}()
                        (LinMRange(1, 10, 10),    LinRange(1, 10, 10),    LinSRange(1, 10, 10)),
                        (StepMRangeLen(1., 1., 10), StepRangeLen(1., 1., 10), StepSRangeLen(1., 1., 10))
                       )
-        @test broadcasted(bstyle, +, mr) == broadcast(+, br)
-        @test broadcasted(bstyle, +, sr) == broadcast(+, br)
+        @testset "" begin
+            @test broadcasted(bstyle, +, mr) ==
+                  broadcast(+, br) ==
+                  broadcasted(bstyle, +, sr)
 
-        @test broadcasted(bstyle, -, mr) ==  broadcast(-, br)
-        @test broadcasted(bstyle, -, sr) ==  broadcast(-, br)
+            @test broadcasted(bstyle, -, mr) ==
+                  broadcast(-, br) ==
+                  broadcasted(bstyle, -, sr)
 
-        @test broadcasted(bstyle, +, 1, mr) == broadcast(+, 1, br)
-        @test broadcasted(bstyle, +, 1, sr) == broadcast(+, 1, br)
+            @test broadcasted(bstyle, +, 1, mr) ==
+                  broadcast(+, 1, br) ==
+                  broadcasted(bstyle, +, 1, sr)
 
-        @test broadcasted(bstyle, -, 1, mr) == broadcast(-, 1, br)
-        @test broadcasted(bstyle, -, 1, sr) == broadcast(-, 1, br)
+            @test broadcasted(bstyle, -, 1, mr) ==
+                  broadcast(-, 1, br) ==
+                  broadcasted(bstyle, -, 1, sr)
 
-        @test broadcasted(bstyle, +, mr, 1) == broadcast(+, br, 1)
-        @test broadcasted(bstyle, +, sr, 1) == broadcast(+, br, 1)
+            @test broadcasted(bstyle, +, mr, 1) ==
+                  broadcast(+, br, 1) ==
+                  broadcasted(bstyle, +, sr, 1)
 
-        @test broadcasted(bstyle, -, mr, 1)  == broadcast(-, br, 1)
-        @test broadcasted(bstyle, -, sr, 1)  == broadcast(-, br, 1)
+            @test broadcasted(bstyle, -, mr, 1) ==
+                  broadcast(-, br, 1) ==
+                  broadcasted(bstyle, -, sr, 1)
 
-        @test broadcasted(bstyle, *, 1, mr) == broadcast(*, 1, br)
-        @test broadcasted(bstyle, *, 1, sr) == broadcast(*, 1, br)
+            @test broadcasted(bstyle, *, 1, mr) ==
+                  broadcast(*, 1, br) ==
+                  broadcasted(bstyle, *, 1, sr)
 
-        @test broadcasted(bstyle, *, mr, 1) == broadcast(*, br, 1)
-        @test broadcasted(bstyle, *, sr, 1) == broadcast(*, br, 1)
+            @test broadcasted(bstyle, *, mr, 1) ==
+                  broadcast(*, br, 1) ==
+                  broadcasted(bstyle, *, sr, 1)
 
-        @test broadcasted(bstyle, \, 1, mr) == broadcast(\, 1, br)
-        @test broadcasted(bstyle, \, 1, sr) == broadcast(\, 1, br)
+            @test broadcasted(bstyle, \, 1, mr) ==
+                  broadcast(\, 1, br) ==
+                  broadcasted(bstyle, \, 1, sr)
 
-        @test /(mr, 2) == /(br, 2)
-        @test /(sr, 2) == /(br, 2)
+            @test /(mr, 2) == /(br, 2) == /(sr, 2)
 
-        @test -(mr, (2 .* br)) == -(br, (2 .* br))
-        @test -(sr, (2 .* br)) == -(br, (2 .* br))
-        @test -((2 .* br), mr) == -((2 .* br), br)
-        @test -((2 .* br), sr) == -((2 .* br), br)
+            @test -(mr, (2 .* br)) == -(br, (2 .* br)) == -(sr, (2 .* br))
+            @test -((2 .* br), mr) == -((2 .* br), br) == -((2 .* br), sr)
 
-        @test +(mr, (1 .+ br)) == +(br, (1 .+ br))
-        @test +(sr, (1 .+ br)) == +(br, (1 .+ br))
-        @test +((1 .+ br), mr) == +((1 .+ br), br)
-        @test +((1 .+ br), sr) == +((1 .+ br), br)
+            @test +(mr, (1 .+ br)) == +(br, (1 .+ br)) == +(sr, (1 .+ br))
+            @test +((1 .+ br), mr) == +((1 .+ br), br) == +((1 .+ br), sr)
 
-        @test *(mr, 2) == *(sr, 2) == *(br, 2)
-        @test *(2, mr) == *(2, sr) == *(2, br)
+            @test *(mr, 2) == *(sr, 2) == *(br, 2)
+            @test *(2, mr) == *(2, sr) == *(2, br)
+        end
     end
 end
